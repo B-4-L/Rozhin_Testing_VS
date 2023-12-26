9aa9c1b233eaf86c9fa37b99c0432750496408b8
B-4-L <147860744+B-4-L@users.noreply.github.com>
2023-12-26 14:59:53 +0300

    Add files via upload

---
+++ Rozhin_Testing_VS/Program/Program/Program.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string> // Добавляем библиотеку для работы со строками
+
+using namespace std;
+
+int Vvestichislo(string s) { // Изменяем функцию, чтобы она принимала строку в качестве аргумента
+    int nomer;
+    try {
+        nomer = stoi(s); // Преобразуем строку в число
+        if (nomer <= 0) { // Проверяем, что число натуральное
+            throw "Отрицательное число или 0";
+        }
+    }
+    catch (...) {
+        return false; // Если произошла ошибка при преобразовании строки в число, возвращаем false
+    }
+    return nomer; // Возвращаем натуральное число
+}
+
+int Maxchislo(int nomer) {
+    int Max = 0;
+    while (nomer > 0) {
+        int cifra = nomer % 10;
+        if (cifra > Max) {
+            Max = cifra;
+        }
+        nomer = nomer / 10;
+    }
+    return Max;
+}
+int Minchislo(int nomer) {
+    int Min = 9;
+    while (nomer > 0) {
+
+        int cifra = nomer % 10;
+        if (cifra < Min) {
+            Min = cifra;
+        }
+        nomer = nomer / 10;
+    }
+    return Min;
+}
+
+int main() {
+    string chislo; // Изменяем тип переменной на string
+    int nomer = 0;
+
+    cout << "Meню выборa:" << endl;
+    cout << "1. Ввести натуральное число X" << endl;
+    cout << "2. Найти наибольшую цифру в записи числа X" << endl;
+    cout << "3. Найти наименьшую цифру в записи числа X" << endl;
+    cout << "Выберите пункт меню: ";
+    cin >> chislo; // Теперь считываем строку
+
+    switch (stoi(chislo)) { // Преобразуем строку в число
+    case 1:
+        nomer = Vvestichislo(chislo); // Передаем строку в функцию
+        break;
+    case 2:
+        nomer = Vvestichislo(chislo);
+        cout << "Наибольшая цифра в записи числа Х: " << Maxchislo(nomer) << std::endl;
+        break;
+    case 3:
+        nomer = Vvestichislo(chislo);
+        cout << "Наименьшая цифра в записи числа Х: " << Minchislo(nomer) << std::endl;
+        break;
+    default:
+        cout << "Heвeрный выбор" << endl;
+        break;
+    }
+
+    return 0;
+}
---
+++ Rozhin_Testing_VS/Program/testing/TestmyProgram/TestmyProgram/TestmyProgram.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <iostream>
+#include <string>
+#include "E:\2 курс\ППО\ППО - 4 ЛБ\Rozhin_Testing_VS\Program\Program\Program.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestmyProgram
+{
+    TEST_CLASS(TestmyProgram)
+    {
+    public:
+
+        TEST_METHOD(Test_Input)
+        {
+            int expected = 20;
+            string s = "20";
+            Assert::AreEqual(expected, Vvestichislo(s), L"Incorrect value");
+        }
+
+        TEST_METHOD(test_vvesti_chislo_negative)
+        {
+            int expected = 0; // Значение, которое будет возвращено при ошибке
+            string s = "a";
+            Assert::AreEqual(expected, Vvestichislo(s), L"Incorrect value");
+        }
+
+        TEST_METHOD(test_vvesti_chislo_zero)
+        {
+            int expected = 0; // Значение, которое будет возвращено при ошибке
+            string s = "-12";
+            Assert::AreEqual(expected, Vvestichislo(s), L"Incorrect value");
+        }
+
+        TEST_METHOD(test_max_cifra)
+        {
+            // Тестирование функции поиска наибольшей цифры
+            int max = Maxchislo(12345);
+            Assert::AreEqual(5, max);
+        }
+
+        TEST_METHOD(test_min_cifra)
+        {
+            // Тестирование функции поиска наименьшей цифры
+            int min = Minchislo(12345);
+            Assert::AreEqual(1, min);
+        }
+
+        TEST_METHOD(test_max_cifra_zero)
+        {
+            // Тестирование функции поиска наибольшей цифры в случае нулевого числа
+            int max = Maxchislo(0);
+            Assert::AreEqual(0, max);
+        }
+
+        TEST_METHOD(test_min_cifra_same_digits)
+        {
+            // Тестирование функции поиска наименьшей цифры в случае всех одинаковых цифр
+            int min = Minchislo(11111);
+            Assert::AreEqual(1, min);
+        }
+    };
+}
